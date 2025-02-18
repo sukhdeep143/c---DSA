406507ae0e6f97256c4b8e9fc0be6ceb764e4097
Sukhdeep Singh <sukhdeepsingh141414@gmail.com>
2025-02-18 09:34:25 +0530

    deletion

--- DSA using cPP/OnlyDeletion.cpp
+++ DSA using cPP/OnlyDeletion.cpp
@@ -3,9 +3,24 @@ using namespace std;
 
 int main(){
     int arr[] = {90,40,30};
-    int i,n = 3;
+    int array_index = 3;
+    int element_want_to_delete = 1;
 
-    for(i = 0; i<n; i++){
+    cout<<"Original Array"<< endl;
+    for(int i = 0; i< array_index; i++){
         cout<< "arr["<<i<<"] = "<< arr[i]<<endl; 
     }
+    
+    for(int i = array_index; i < array_index-1; i++){
+        arr[i]  = arr[i + 1];
+    }
+    
+  array_index--;
+    cout<<"After Deleting the element of Array"<< endl;
+    for (int i = 0; i < array_index; i++) {
+        cout<< "arr["<<i<<"] = "<< arr[i]<<endl; 
+    }
+    cout << endl;
+
+    return 0;
 }
