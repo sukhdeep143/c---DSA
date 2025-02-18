cdf9faff1e974bd6c51cbb9a2e4f13f7e758470e
Sukhdeep Singh <sukhdeepsingh141414@gmail.com>
2025-02-18 09:45:15 +0530

    bug fix

--- DSA using cPP/OnlyDeletion.cpp
+++ DSA using cPP/OnlyDeletion.cpp
@@ -1,24 +1,26 @@
 #include <iostream>
 using namespace std; 
 
-int main(){
-    int arr[] = {90,40,30};
-    int array_index = 3;
-    int element_want_to_delete = 1;
+int main() {
+    int arr[] = {90, 40, 30};
+    int array_index = 3; // Size of the array
+    int element_want_to_delete = 1; // Index of the element to delete
 
-    cout<<"Original Array"<< endl;
-    for(int i = 0; i< array_index; i++){
-        cout<< "arr["<<i<<"] = "<< arr[i]<<endl; 
-    }
-    
-    for(int i = array_index; i < array_index-1; i++){
-        arr[i]  = arr[i + 1];
-    }
-    
-  array_index--;
-    cout<<"After Deleting the element of Array"<< endl;
+    cout << "Original Array:" << endl;
     for (int i = 0; i < array_index; i++) {
-        cout<< "arr["<<i<<"] = "<< arr[i]<<endl; 
+        cout << "arr[" << i << "] = " << arr[i] << endl;
+    }
+
+    // Shift elements to the left
+    for (int i = element_want_to_delete; i < array_index - 1; i++) {
+        arr[i] = arr[i + 1];
+    }
+
+    array_index--; // Reduce the size of the array
+
+    cout << "After Deleting the element from the Array:" << endl;
+    for (int i = 0; i < array_index; i++) {
+        cout << "arr[" << i << "] = " << arr[i] << endl;
     }
     cout << endl;
 
